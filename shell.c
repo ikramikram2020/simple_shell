17f2db266b0a15ebf5b960852f0dc0e12a1caaf3
Boutheldja Kaouther <kaoutherboutheldja@gmail.com>
2023-12-01 03:13:22 -0800

    Add shell.c file

---
+++ shell.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+/**
+ * main - main shell to execute commands
+ *
+ * @argv: array of strings of arguments
+ * @argc: number of arguments
+ * @env: environment variables
+ *
+ * Return: on success 0, -1 on failure
+ */
+
+int main( char **argv, int argc, char **env)
+{
+	int count = 1;
+	/* */
+	(void)argc;
+
+	return (0);
+}
--- shell.h
+++ shell.h
@@ -1,6 +1,15 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+void _print_prompt(void);
 
 
 #endif /* SHELL_H */
