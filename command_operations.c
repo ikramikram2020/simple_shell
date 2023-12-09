04983952df121accde8cb3fd0e2e1bdca2fc6248
kawthar <kawtharikram2020@gmail.com>
2023-12-09 13:04:50 -0800

    shell

--- command_operations.c
+++ command_operations.c
@@ -11,35 +11,23 @@
 
 int _count_commands(const char *command)
 {
-        char *command_copy;
-        int command_count = 0;
-        char *token;
-
-        if (command == NULL)
-                return (0);
-
-        command_copy = _stringdup(command);
-        if (command_copy == NULL)
-        {
-                return (0);
-        }
-
-        token = strtok(command_copy, " \t\n");
-        while (token != NULL)
-        {
-                command_count++;
-                token = strtok(NULL, " \t\n");
-        }
-
-        free(command_copy);
-        return (command_count);
+char *command_copy;
+int command_count = 0;
+char *token;
+if (command == NULL)
+return (0);
+command_copy = _stringdup(command);
+if (command_copy == NULL)
+{
+return (0);
+}
+token = strtok(command_copy, " \t\n");
+while (token != NULL)
+{
+command_count++;
+token = strtok(NULL, " \t\n");
+}
+free(command_copy);
+return (command_count);
 }
 
-/**
- * _create_command - Creates an array of strings from a command.
- *
- * @command: Command string.
- *
- * Return: Array of strings representing the full command.
- */
-char **_create_command(const char *command);
