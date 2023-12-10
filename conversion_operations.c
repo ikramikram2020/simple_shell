e023d18f05112beb9e4d1f462b8b9aae52ad0d8d
Boutheldja Kaouther <kaoutherboutheldja@gmail.com>
2023-12-10 07:18:31 -0800

    add fuctions

--- conversion_operations.c
+++ conversion_operations.c
@@ -38,5 +38,34 @@ return (num);
  *
  * Return: String representation of the integer.
  */
-char *_int_to_string(int number);
+char *_int_to_string(int number)
+{
+int nbr_digits = 0, temp;
+char *string_int;
 
+temp = number;
+if (number == 0)
+nbr_digits = 1;
+else
+{
+while (number != 0)
+{
+number /= 10;
+nbr_digits++;
+}
+}
+string_int = malloc(sizeof(char) * (nbr_digits + 1));
+if (string_int == NULL)
+{
+perror("malloc");
+return (NULL);
+}
+string_int[nbr_digits] = '\0';
+while (nbr_digits > 0)
+{
+string_int[nbr_digits - 1] = '0' + (temp % 10);
+temp /= 10;
+nbr_digits--;
+}
+return (string_int);
+}
