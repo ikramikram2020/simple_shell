aea20d5bd83358a997c5d1dc7843790457e54e3a
Boutheldja Kaouther <kaoutherboutheldja@gmail.com>
2023-12-10 08:04:24 -0800

    add some changes

--- shell.c
+++ shell.c
@@ -12,7 +12,7 @@
  */
 int main(int argc, char **argv, char **env)
 {
-int counter = 1;
+int count = 1;
 (void)argc;
 
 while (1)
@@ -27,29 +27,29 @@ if (get_input[0] == '\n' || get_input == NULL)
 free(get_input);
 continue;
 }
-full_command = _create_full_command(get_input);
+full_command = _create_command(get_input);
 free(get_input);
 get_input = NULL;
 if (full_command == NULL)
 continue;
 if (_check_exit(full_command) == -1)
 {
-_error_handler(argv[0], counter, full_command, 0);
-_free_full_command(full_command);
+_error_handler(argv[0], count, full_command, 0);
+_free_command(full_command);
 if (!isatty(STDIN_FILENO))
 exit(2);
-counter++;
+count++;
 continue;
 }
-if (_stringncmp(full_command[0], "env", 3) == 0)
+if (_strncmp(full_command[0], "env", 3) == 0)
 {
 _print_env(env);
 continue;
 }
-_execute_command(argv, full_command, counter, env);
-_free_full_command(full_command);
+_execute_command(argv, full_command, count, env);
+_free_command(full_command);
 full_command = NULL;
-counter++;
+count++;
 }
 return (0);
 }
