655d38871a5fd95f78812638cb3dba196741a384
Boutheldja Kaouther <kaoutherboutheldja@gmail.com>
2023-12-10 07:15:31 -0800

    add fuctions

--- conversion_operations.c
+++ conversion_operations.c
@@ -9,7 +9,27 @@
  *
  * Return: Integer value.
  */
-long int _atoi(char *string);
+long int _atoi(char *string)
+{
+int num = 0, count = 0;
+
+if (string == NULL)
+return (-1);
+if (!string[1])
+{
+num = string[0] - '0';
+return (num);
+}
+while (string[count] != '\0')
+{
+if (string[count] >= '0' && string[count] <= '9')
+num = (num * 10) + (string[count] - '0');
+else
+return (-1);
+count++;
+}
+return (num);
+}
 
 /**
  * _int_to_string - Converts an integer to a string.
