f50274a3d23412ba49f6055e098116a8c0e9a606
kawthar <kawtharikram2020@gmail.com>
2023-12-10 13:27:12 -0800

    shell

--- error_operations.c
+++ error_operations.c
@@ -12,25 +12,27 @@
  */
 void _error_handler(char *name, int count, char **command, int status)
 {
-char *string_counter;
+	char *string_counter;
 
-string_counter = _itoa(count);
-write(STDERR_FILENO, name, _strlen(name));
-write(STDERR_FILENO, ": ", 2);
-write(STDERR_FILENO, string_counter, _strlen(string_counter));
-write(STDERR_FILENO, ": ", 2);
-write(STDERR_FILENO, command[0], _strlen(command[0]));
-write(STDERR_FILENO, ": ", 2);
-if (status == 1)
-write(STDERR_FILENO, "not found", 9);
-else
-{
-write(STDERR_FILENO, "Illegal number", 14);
-write(STDERR_FILENO, ": ", 2);
-write(STDERR_FILENO, command[1], _strlen(command[1]));
-}
-write(STDERR_FILENO, "\n", 1);
-free(string_counter);
+	string_counter = _itoa(count);
+	write(STDERR_FILENO, name, _strlen(name));
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, string_counter, _strlen(string_counter));
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, command[0], _strlen(command[0]));
+	write(STDERR_FILENO, ": ", 2);
+
+	if (status == 1)
+		write(STDERR_FILENO, "not found", _strlen("not found"));
+	else
+	{
+		write(STDERR_FILENO, "Illegal number", _strlen("Illegal number"));
+		write(STDERR_FILENO, ": ", 2);
+		write(STDERR_FILENO, command[1], _strlen(command[1]));
+	}
+
+	write(STDERR_FILENO, "\n", 1);
+	free(string_counter);
 }
 
 /**
@@ -38,26 +40,27 @@ free(string_counter);
  *
  * @full_command: Array of strings representing the full command.
  *
- * Return: -1 if invalid number, 0 on success.
+ * Return: -1 if an invalid number, 0 on success.
  */
 int _check_exit(char **full_command)
 {
-long int status;
+	long int status;
 
-if (_strncmp(full_command[0], "exit", 4) == 0 && !full_command[1])
-{
-_free_command(full_command);
-exit(0);
+	if (_strncmp(full_command[0], "exit", 4) == 0 && !full_command[1])
+	{
+		_free_command(full_command);
+		exit(0);
+	}
+
+	status = _atoi(full_command[1]);
+	if (_strncmp(full_command[0], "exit", 4) == 0 && status != -1)
+	{
+		_free_command(full_command);
+		exit(status);
+	}
+	else if (_strncmp(full_command[0], "exit", 4) != 0)
+		return (1);
+	else
+		return (-1);
 }
-status = _atoi(full_command[1]);
-if (_strncmp(full_command[0], "exit", 4) == 0
-&& status != -1)
-{
-_free_command(full_command);
-exit(status);
-}
-else if (_strncmp(full_command[0], "exit", 4) != 0)
-return (1);
-else
-return (-1);
-}
+
