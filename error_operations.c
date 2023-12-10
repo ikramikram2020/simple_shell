770dff2667bc166d0b88ed5185236ee783f98e7d
Boutheldja Kaouther <kaoutherboutheldja@gmail.com>
2023-12-10 07:23:27 -0800

    add fuctions

--- error_operations.c
+++ error_operations.c
@@ -10,7 +10,28 @@
  * @command: Array of strings representing the command.
  * @status: Error status (0 for exit, 1 otherwise).
  */
-void _error_handler(char *name, int count, char **command, int status);
+void _error_handler(char *name, int count, char **command, int status)
+{
+char *string_counter;
+
+string_counter = __itoa(count);
+write(STDERR_FILENO, name, _strlen(name));
+write(STDERR_FILENO, ": ", 2);
+write(STDERR_FILENO, string_counter, _strlen(string_counter));
+write(STDERR_FILENO, ": ", 2);
+write(STDERR_FILENO, command[0], _strlen(command[0]));
+write(STDERR_FILENO, ": ", 2);
+if (status == 1)
+write(STDERR_FILENO, "not found", 9);
+else
+{
+write(STDERR_FILENO, "Illegal number", 14);
+write(STDERR_FILENO, ": ", 2);
+write(STDERR_FILENO, command[1], _strlen(command[1]));
+}
+write(STDERR_FILENO, "\n", 1);
+free(string_counter);
+}
 
 /**
  * _check_exit - Checks if the command is an exit command.
